6b2ee0cf76829a4d80d085bf998ac5fb81b62d18
kusano <kusano.hatsumi@gmail.com>
2023-01-18 09:55:40 +0900

    修正

--- src/compornents/App.js
+++ src/compornents/App.js
@@ -5,30 +5,25 @@ import { Form } from "./Form";
 
 import "../scss/_develop/asset.scss";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import Detail from "./Detail";
 import { Send } from "./Send";
 import { Login } from "./Login";
 import { New } from "./New";
-
-
 function App() {
   return (
-    <Router>
     <div className="App">
-      
       <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          {/* <Route path="/About" element={<Detail />} />
+          <Route path="/About" element={<Detail />} />
           <Route path="/recruitform" element={<Form />} />
           <Route path="/send" element={<Send />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/new" element={<New />} /> */}
+          <Route path="/new" element={<New />} />
         </Routes>
       <Footer />
     </div>
-    </Router>
   );
 }
 
