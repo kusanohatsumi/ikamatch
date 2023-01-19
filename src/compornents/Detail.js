fcf89a0dde49b446c68cbb802e15f21ab1681124
kusano <kusano.hatsumi@gmail.com>
2023-01-19 15:47:27 +0900

    build

--- src/compornents/Detail.js
+++ src/compornents/Detail.js
@@ -17,7 +17,7 @@ export const Detail = () => {
         if (flg) {
           flg=false;
           detailItem[i].classList.remove("open");
-          AppBtn.classList.remove("add");
+          // AppBtn.classList.remove("add");
         } else {
           flg=true;
           detailItem[i].classList.add("open");
@@ -39,15 +39,15 @@ export const Detail = () => {
             <p className="name">suda</p>
             <p className="friend-cord">フレンドコード:3044-1210-1751</p>
           </div>
-          <h3 className="ttlh">〇〇したいスプラ勢集まれ！深夜のイカタコマッチ</h3>
+          <h3 className="ttlh">今から遊べるスプラ勢集まれ！深夜のイカタコマッチ</h3>
           <div className="tag"></div>
-          <p className="comment">ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。</p>
+          <p className="comment">暇な人募！深夜のカスタムマッチ　夜更かしさんいらっしゃい</p>
           <div className='AppBtn'>
             <button type='submit'>応募する</button>
           </div>
         </div>
         <figure className='Img'>
-          <img src="https://source.unsplash.com/random/" alt='thumbnail' />
+          <img src={`${process.env.PUBLIC_URL}/img/stage/mahimahi.jpg`} alt='thumbnail' />
         </figure>
       </div>
 
@@ -55,21 +55,77 @@ export const Detail = () => {
         <div id='detailText' className='text'>
           <div className="icon">
             <figure></figure>
-            <p className="name">suda</p>
+            <p className="name">kusano</p>
             <p className="friend-cord">フレンドコード:3044-1210-1751</p>
           </div>
-          <h3 className="ttlh">〇〇したいスプラ勢集まれ！深夜のイカタコマッチ</h3>
+          <h3 className="ttlh">ゆったりまったりカジュアルマッチ</h3>
           <div className="tag"></div>
-          <p className="comment">ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。ここに詳細のコメントを掲載する。</p>
+          <p className="comment">カジュアルに楽しく遊べる人来て欲しいです。カスタムマッチを楽しもう！</p>
           <div className='AppBtn'>
             <button type='submit'>応募する</button>
           </div>
         </div>
         <figure className='Img'>
-          <img src="https://source.unsplash.com/random/" alt='thumbnail' />
+          <img src={`${process.env.PUBLIC_URL}/img/stage/ama.jpg`} />
         </figure>
       </div>
 
+      <div className='detailItem' onClick={openDetail}>
+        <div id='detailText' className='text'>
+          <div className="icon">
+            <figure></figure>
+            <p className="name">kusano</p>
+            <p className="friend-cord">フレンドコード:3044-1210-1751</p>
+          </div>
+          <h3 className="ttlh">ガチ勢歓迎！本気のスプラ</h3>
+          <div className="tag"></div>
+          <p className="comment">大会優勝を目指した練習がしたい！レベルの高いゲームをしたい人集まれ</p>
+          <div className='AppBtn'>
+            <button type='submit'>応募する</button>
+          </div>
+        </div>
+        <figure className='Img'>
+          <img src={`${process.env.PUBLIC_URL}/img/stage/mategai.jpg`} />
+        </figure>
+      </div>
+
+      <div className='detailItem' onClick={openDetail}>
+        <div id='detailText' className='text'>
+          <div className="icon">
+            <figure></figure>
+            <p className="name">kusano</p>
+            <p className="friend-cord">フレンドコード:3044-1210-1751</p>
+          </div>
+          <h3 className="ttlh">主初心者です。練習カスタム</h3>
+          <div className="tag"></div>
+          <p className="comment">初心者のみんなでわいわ楽しくエンジョイカスタム。スプラが苦手な人や始めたての人でも大歓迎！！</p>
+          <div className='AppBtn'>
+            <button type='submit'>応募する</button>
+          </div>
+        </div>
+        <figure className='Img'>
+          <img src={`${process.env.PUBLIC_URL}/img/stage/tyouzame.jpg`} />
+        </figure>
+      </div>
+
+      <div className='detailItem' onClick={openDetail}>
+        <div id='detailText' className='text'>
+          <div className="icon">
+            <figure></figure>
+            <p className="name">kusano</p>
+            <p className="friend-cord">フレンドコード:3044-1210-1751</p>
+          </div>
+          <h3 className="ttlh">「第3回あゆん主催スプラ3大会ビッグプレゼント杯」優勝目指して</h3>
+          <div className="tag"></div>
+          <p className="comment"></p>
+          <div className='AppBtn'>
+            <button type='submit'>応募する</button>
+          </div>
+        </div>
+        <figure className='Img'>
+          <img src={`${process.env.PUBLIC_URL}/img/stage/yagara.jpg`} />
+        </figure>
+      </div>
 
 
 
