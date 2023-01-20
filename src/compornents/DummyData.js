1cb55db02ff1c20447999f8ddfb0d1e252a541ad
kusano <kusano.hatsumi@gmail.com>
2023-01-20 12:17:12 +0900

    build

--- src/compornents/DummyData.js
+++ src/compornents/DummyData.js
@@ -8,7 +8,7 @@ export const PostsData = [
         text:"今まで使ったことのないブキを練習したいです！どんなブキ使ってもいいよって人お願いします！",
         mode:"asari",
         xp:"1600",
-        area:"gonnzui.jpg",
+        rea:"gonnzui.jpg",
     }
     ,
     {
@@ -20,7 +20,7 @@ export const PostsData = [
         text : "ガチアサリが死ぬほど苦手です。克服したいのでガチアサリ練習に付き合ってくださる方を募集してます。",
         mode:"asari",
         xp:"1600",
-        area:"masaba.jpg",
+        rea:"masaba.jpg",
     }
     ,
     {
@@ -32,35 +32,9 @@ export const PostsData = [
         text : "スプラで遊んでくれる友達がいないので友達募集中です。おしゃべりしながらまったり遊びたいです。",
         mode:"asari",
         xp:"1600",
-        area : "tyouzame.jpg",
+        rea : "tyouzame.jpg",
     }
     ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "may",
-        code: "4324-4595-3467",
-        title : "スプラ女子会",
-        text : "深夜に一緒に遊んでくれる人募集中！女子のスプラ仲間が欲しいです！",
-        mode:"asari",
-        xp:"1600",
-        area : "yunohana.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "ライト",
-        code: "4637-6847-3945",
-        title : "まったり深夜スプラ",
-        text : "平日深夜の夜更かしに一緒にスプラしてくれる人探してます。",
-        mode:"asari",
-        xp:"1600",
-        area : "yunohana.jpg",
-    }
-    ,
-// ---
-
 
 ]
     export default PostsData;
