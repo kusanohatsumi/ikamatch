ab51a13c03742236d6cde9f616e94545374901e9
kusano <kusano.hatsumi@gmail.com>
2023-01-20 12:13:28 +0900

    build

--- src/compornents/DummyData.js
+++ src/compornents/DummyData.js
@@ -59,77 +59,8 @@ export const PostsData = [
         area : "yunohana.jpg",
     }
     ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "つくし",
-        code: "3472-8457-4573",
-        title : "カスタムマッチ募集",
-        text : "楽しく遊んでくださる方〜いませんか〜？",
-        mode:"asari",
-        xp:"1600",
-        area : "namerou.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "ある",
-        code: "3748-3475-3459",
-        title : "ホクサイ祭り開催",
-        text : "ホクサイ縛りでわちゃわちゃしたいです！",
-        mode:"asari",
-        xp:"1600",
-        area : "mategai.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "七兵衛",
-        code: "3645-7546-3495",
-        title : "配信者限定カスタム",
-        text : "スプラの配信者集まれ〜〜",
-        mode:"asari",
-        xp:"1600",
-        area : "tyouzame.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "りょ〜ゆ〜",
-        code: "3957-4853-2945",
-        title : "フェス前練習！！！",
-        text : "フェスで〇〇陣営の方、一緒に遊びませんか？",
-        mode:"asari",
-        xp:"1600",
-        area : "masaba.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "yuki",
-        code: "9348-4573-4592",
-        title : "初心者、カスタムマッチ練習",
-        text : "ガチマッチの立ち回りを練習したいので、初心者でもいいよ〜って方お願いします",
-        mode:"asari",
-        xp:"1600",
-        area : "namerou.jpg",
-    }
-    ,
-    {
-        id:"",
-        icon: "icon.svg" ,
-        nickname : "夏葉",
-        code: "7324-3958-3475",
-        title : "リア充爆破カスタム",
-        text : "クリスマスにボッチなのが辛いので誰か慰めてください",
-        mode:"asari",
-        xp:"1600",
-        area : "namerou.jpg",
-    }
-    ,
+// ---
+
+
 ]
     export default PostsData;
